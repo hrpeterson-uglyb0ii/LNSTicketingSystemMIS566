f412eaa685d6d93ccdf4eab0c07f3c043d0c4fca
hrpeterson-uglyb0ii <hrpeterson@ksu.edu>
2023-11-07 13:50:10 -0600

    testing push to branch

---
+++ TicketManagementSystem/ProfessorView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketManagementSystem
+{
+    public partial class ProfessorView : Form
+    {
+        public ProfessorView()
+        {
+            InitializeComponent();
+        }
+
+        private void logOffButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ProfessorView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+            stview stViewForm = new stview();
+            stViewForm.Show();
+        }
+
+        private void viewButton2_Click(object sender, EventArgs e)
+        {
+            Leaderboard leaderboard = new Leaderboard();
+            leaderboard.Show();
+        }
+    }
+}
---
+++ TicketManagementSystem/TAview.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace TicketManagementSystem
+{
+    partial class TAview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TAview));
+            this.groupBoxResolve = new System.Windows.Forms.GroupBox();
+            this.resolvePictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resolveLabel = new System.Windows.Forms.Label();
+            this.groupBoxView = new System.Windows.Forms.GroupBox();
+            this.viewPictureBox = new System.Windows.Forms.PictureBox();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.viewLabel = new System.Windows.Forms.Label();
+            this.logOffButton = new System.Windows.Forms.Button();
+            this.TAlabel = new System.Windows.Forms.Label();
+            this.groupBoxResolve.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resolvePictureBox)).BeginInit();
+            this.groupBoxView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.viewPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxResolve
+            // 
+            this.groupBoxResolve.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBoxResolve.Controls.Add(this.resolvePictureBox);
+            this.groupBoxResolve.Controls.Add(this.button1);
+            this.groupBoxResolve.Controls.Add(this.resolveLabel);
+            this.groupBoxResolve.Location = new System.Drawing.Point(517, 137);
+            this.groupBoxResolve.Name = "groupBoxResolve";
+            this.groupBoxResolve.Size = new System.Drawing.Size(283, 397);
+            this.groupBoxResolve.TabIndex = 3;
+            this.groupBoxResolve.TabStop = false;
+            // 
+            // resolvePictureBox
+            // 
+            this.resolvePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("resolvePictureBox.Image")));
+            this.resolvePictureBox.Location = new System.Drawing.Point(52, 110);
+            this.resolvePictureBox.Name = "resolvePictureBox";
+            this.resolvePictureBox.Size = new System.Drawing.Size(189, 165);
+            this.resolvePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.resolvePictureBox.TabIndex = 4;
+            this.resolvePictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(31, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Resolve";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // resolveLabel
+            // 
+            this.resolveLabel.AutoSize = true;
+            this.resolveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resolveLabel.Location = new System.Drawing.Point(25, 31);
+            this.resolveLabel.Name = "resolveLabel";
+            this.resolveLabel.Size = new System.Drawing.Size(249, 32);
+            this.resolveLabel.TabIndex = 2;
+            this.resolveLabel.Text = "Resolve Ticket(s)";
+            // 
+            // groupBoxView
+            // 
+            this.groupBoxView.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBoxView.Controls.Add(this.viewPictureBox);
+            this.groupBoxView.Controls.Add(this.viewButton);
+            this.groupBoxView.Controls.Add(this.viewLabel);
+            this.groupBoxView.Location = new System.Drawing.Point(189, 137);
+            this.groupBoxView.Name = "groupBoxView";
+            this.groupBoxView.Size = new System.Drawing.Size(283, 397);
+            this.groupBoxView.TabIndex = 4;
+            this.groupBoxView.TabStop = false;
+            // 
+            // viewPictureBox
+            // 
+            this.viewPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("viewPictureBox.Image")));
+            this.viewPictureBox.Location = new System.Drawing.Point(46, 110);
+            this.viewPictureBox.Name = "viewPictureBox";
+            this.viewPictureBox.Size = new System.Drawing.Size(189, 165);
+            this.viewPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.viewPictureBox.TabIndex = 3;
+            this.viewPictureBox.TabStop = false;
+            // 
+            // viewButton
+            // 
+            this.viewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewButton.Location = new System.Drawing.Point(30, 340);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(225, 46);
+            this.viewButton.TabIndex = 2;
+            this.viewButton.Text = "View";
+            this.viewButton.UseVisualStyleBackColor = true;
+            // 
+            // viewLabel
+            // 
+            this.viewLabel.AutoSize = true;
+            this.viewLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewLabel.Location = new System.Drawing.Point(40, 31);
+            this.viewLabel.Name = "viewLabel";
+            this.viewLabel.Size = new System.Drawing.Size(205, 32);
+            this.viewLabel.TabIndex = 1;
+            this.viewLabel.Text = "View Ticket(s)";
+            // 
+            // logOffButton
+            // 
+            this.logOffButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logOffButton.Location = new System.Drawing.Point(825, 12);
+            this.logOffButton.Name = "logOffButton";
+            this.logOffButton.Size = new System.Drawing.Size(142, 40);
+            this.logOffButton.TabIndex = 5;
+            this.logOffButton.Text = "Log Off";
+            this.logOffButton.UseVisualStyleBackColor = true;
+            this.logOffButton.Click += new System.EventHandler(this.logOffButton_Click);
+            // 
+            // TAlabel
+            // 
+            this.TAlabel.AutoSize = true;
+            this.TAlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TAlabel.Location = new System.Drawing.Point(151, 64);
+            this.TAlabel.Name = "TAlabel";
+            this.TAlabel.Size = new System.Drawing.Size(716, 46);
+            this.TAlabel.TabIndex = 6;
+            this.TAlabel.Text = "Teaching Assistant Ticket Dashboard";
+            // 
+            // TAview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 564);
+            this.Controls.Add(this.TAlabel);
+            this.Controls.Add(this.logOffButton);
+            this.Controls.Add(this.groupBoxResolve);
+            this.Controls.Add(this.groupBoxView);
+            this.Name = "TAview";
+            this.Text = "TAview";
+            this.groupBoxResolve.ResumeLayout(false);
+            this.groupBoxResolve.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resolvePictureBox)).EndInit();
+            this.groupBoxView.ResumeLayout(false);
+            this.groupBoxView.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.viewPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxResolve;
+        private System.Windows.Forms.PictureBox resolvePictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resolveLabel;
+        private System.Windows.Forms.GroupBox groupBoxView;
+        private System.Windows.Forms.PictureBox viewPictureBox;
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.Label viewLabel;
+        private System.Windows.Forms.Button logOffButton;
+        private System.Windows.Forms.Label TAlabel;
+    }
+}
